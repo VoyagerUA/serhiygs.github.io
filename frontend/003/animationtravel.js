4f982d1649bc74af898e71c02ada34ba032d3177
Serhiy <123894736+SerhiyGS@users.noreply.github.com>
2023-05-01 03:28:15 +0300

    Add files via upload

--- frontend/003/animationtravel.js
+++ frontend/003/animationtravel.js
@@ -53,205 +53,218 @@ function arrOval(startX, startY, radiusX, radiusY, vector, speed){
 
 // ************************************** ПОЧАТОК ВЕРСІЇ №3 ******************************************************
 function StartAnimationTravel(address){
-  fetch(`${address}/goals.json`)
-  .then(response => response.json())
-  .then(data => {
-    // Розшифровуємо дані з файлу та зберігаємо в змінну
-    arrWagons  = JSON.parse(JSON.stringify(data));
-    numtask = 0;
-    InitObjects();
-  })
-  .catch(error => console.error(error));
+  if(FlagStart){
+
+    fetch(`${address}/goals.json`)
+    .then(response => response.json())
+    .then(data => {
+      // Розшифровуємо дані з файлу та зберігаємо в змінну
+      arrWagons  = JSON.parse(JSON.stringify(data));
+      numtask = 0;
+      InitObjects();
+    })
+    .catch(error => console.error(error));
+  }  
 }
 // ---------------------------------------------------------------------
 function InitObjects(){
-  FlagStart = true;
-  parentElement = document.querySelector('.parentblock');
-  parentElement.style.padding = 0;
-  parentElement.style.boxSizing = `border-box`;
-  parentElement.style.overflow = 'hidden';
-  parentElement.style.backgroundColor = 'black';
+  if(FlagStart){
 
-  pw = parentElement.offsetWidth;
-  ph = parentElement.offsetHeight;
-  pmax = (pw>=ph)?pw:ph;
-  pmin = (pw<=ph)?pw:ph;
- 
-  SceneMake();
+    parentElement = document.querySelector('.parentblock');
+    parentElement.style.padding = 0;
+    parentElement.style.boxSizing = `border-box`;
+    parentElement.style.overflow = 'hidden';
+    parentElement.style.backgroundColor = 'black';
 
-  PersonPaint('items/standingperson.png','visible');
-  //PersonPaint('anime/kydok2.gif','visible');
- 
-  ThingPaint('items/papkaG.png','hidden');
-  TasksBlocks();
-  setTimeout(() => {
-    TaskMove();
-  }, 1000);
+    pw = parentElement.offsetWidth;
+    ph = parentElement.offsetHeight;
+    pmax = (pw>=ph)?pw:ph;
+    pmin = (pw<=ph)?pw:ph;
+  
+    SceneMake();
 
+    PersonPaint('items/standingperson.png','visible');
+    //PersonPaint('anime/kydok2.gif','visible');
+  
+    ThingPaint('items/papkaG.png','hidden');
+    TasksBlocks();
+    setTimeout(() => {
+      TaskMove();
+    }, 1000);
+  }
 
 }
 // ---------------------------------------------------------------
 // Створення і відображення сцени та персони з предметом
 function SceneMake(){
-  //  cancelAnimationFrame(animationIdWagonAnimate);
-    parentElement.innerHTML = '';
-    scene = document.createElement('div');
-    scene.id = 'scene_animationtravel';
-    scene.style.position= 'relative';
-    scene.style.width = '100%';
-    scene.style.height = '100%';
-    //scene.style.backgroundColor = `rgb(47, 107, 92)`;
-    scene.style.overflow = 'hidden';
-    //scene.style.border = '1px solid red';
-    /*
-    scene.style.background = `
-    linear-gradient(to bottom, rgba(30, 150, 255, 1) 0%, 
-    rgba(175, 238, 238, 1) 20%, 
-    rgba(155, 200, 200, 1) 25%, 
-    rgba(220, 220, 220, 0.4) 60%, 
-    rgba(128, 128, 128, 0.4) 100%)`;
-    */
-  
-    scene.style.background = `
-    linear-gradient(to bottom, 
-    rgba(180, 210, 240, 1) 0%, 
-    rgba(150, 150, 150, 0.9) 100%)`;
+  if(FlagStart){
+
+    //  cancelAnimationFrame(animationIdWagonAnimate);
+      parentElement.innerHTML = '';
+      scene = document.createElement('div');
+      scene.id = 'scene_animationtravel';
+      scene.style.position= 'relative';
+      scene.style.width = '100%';
+      scene.style.height = '100%';
+      //scene.style.backgroundColor = `rgb(47, 107, 92)`;
+      scene.style.overflow = 'hidden';
+      //scene.style.border = '1px solid red';
+      /*
+      scene.style.background = `
+      linear-gradient(to bottom, rgba(30, 150, 255, 1) 0%, 
+      rgba(175, 238, 238, 1) 20%, 
+      rgba(155, 200, 200, 1) 25%, 
+      rgba(220, 220, 220, 0.4) 60%, 
+      rgba(128, 128, 128, 0.4) 100%)`;
+      */
     
-    parentElement.appendChild(scene);
-  
-/*  
-*/    
+      scene.style.background = `
+      linear-gradient(to bottom, 
+      rgba(180, 210, 240, 1) 0%, 
+      rgba(150, 150, 150, 0.9) 100%)`;
+      
+      parentElement.appendChild(scene);
+    }  
+
   }
 // --------------------------------------------------------------------------------
 
 // Промальовка персони
 function PersonPaint(fotka, pokaz){
+  if(FlagStart){
 
-  //Створення персони  
-  person = document.createElement('div');
-  person.id = `person_animationtravel`;
-  scene.appendChild(person);
-  person = document.getElementById('person_animationtravel');
+    //Створення персони  
+    person = document.createElement('div');
+    person.id = `person_animationtravel`;
+    scene.appendChild(person);
+    person = document.getElementById('person_animationtravel');
 
-  //let personLeft = 45;
-  let personBottom = '0';
-  let personWidth = 20;
-  let personHeight = 18.2;
-  //let personBgColor = 'orange';
+    //let personLeft = 45;
+    let personBottom = '0';
+    let personWidth = 20;
+    let personHeight = 18.2;
+    //let personBgColor = 'orange';
 
-  person.style.position = 'absolute';
+    person.style.position = 'absolute';
 
-  //person.style.left = personLeft*(pw * 0.01)+'px';
-
-
-  //person.style.bottom = personBottom*(ph * 0.015)+'px';
-  person.style.bottom = `${personBottom}px`;
-
-  person.style.width = personWidth*(pmin * 0.025)+'px';
-  person.style.height = personHeight*(pmin * 0.025)+'px';
-
-  person.style.left = (scene.offsetWidth / 2) - (person.offsetWidth / 2) + 'px';
+    //person.style.left = personLeft*(pw * 0.01)+'px';
 
 
-  //person.style.backgroundColor = personBgColor;
-//  person.style.backgroundImage = `url('./img/kydok1.gif')`;
-  person.style.backgroundImage = `url('./img/${fotka}')`;
+    //person.style.bottom = personBottom*(ph * 0.015)+'px';
+    person.style.bottom = `${personBottom}px`;
 
-  person.style.backgroundSize = `cover`;
-  person.style.backgroundRepeat = `no-repeat`;
-  person.style.backgroundPosition = `bottom`;
+    person.style.width = personWidth*(pmin * 0.025)+'px';
+    person.style.height = personHeight*(pmin * 0.025)+'px';
 
-  //person.style.border = `1px solid red`;
+    person.style.left = (scene.offsetWidth / 2) - (person.offsetWidth / 2) + 'px';
 
-  person.style.visibility = pokaz;
+
+    //person.style.backgroundColor = personBgColor;
+  //  person.style.backgroundImage = `url('./img/kydok1.gif')`;
+    person.style.backgroundImage = `url('./img/${fotka}')`;
+
+    person.style.backgroundSize = `cover`;
+    person.style.backgroundRepeat = `no-repeat`;
+    person.style.backgroundPosition = `bottom`;
+
+    //person.style.border = `1px solid red`;
+
+    person.style.visibility = pokaz;
+  }  
 }
 
 // Промальовка предмета
 function ThingPaint(fotka, pokaz){
+  if(FlagStart){
 
-  //Створення предмету
-  thing = document.createElement('div');
-  thing.id = `thing_animationtravel`;
-  scene.appendChild(thing);
-  thing = document.getElementById('thing_animationtravel');
+    //Створення предмету
+    thing = document.createElement('div');
+    thing.id = `thing_animationtravel`;
+    scene.appendChild(thing);
+    thing = document.getElementById('thing_animationtravel');
 
-  let thingWidth = 5;
-  let thingHeight = 4;
+    let thingWidth = 5;
+    let thingHeight = 4;
 
-  thing.style.position = 'absolute';
-  thing.style.display = `flex`;
+    thing.style.position = 'absolute';
+    thing.style.display = `flex`;
 
-  //let thingLeft = person.offsetLeft + person.offsetWidth;
-  //let thingLeft = person.offsetLeft;
+    //let thingLeft = person.offsetLeft + person.offsetWidth;
+    //let thingLeft = person.offsetLeft;
 
-  let thingLeft = person.offsetLeft + person.offsetWidth / 4;
-  thing.style.left = thingLeft + 'px';
-  thing.style.width = thingWidth*(pmin * 0.025)+'px';
-  thing.style.height = thingHeight*(pmin * 0.025)+'px';
+    let thingLeft = person.offsetLeft + person.offsetWidth / 4;
+    thing.style.left = thingLeft + 'px';
+    thing.style.width = thingWidth*(pmin * 0.025)+'px';
+    thing.style.height = thingHeight*(pmin * 0.025)+'px';
 
-  thing.style.transition = "left 0.5s ease-out, top 0.5s ease-out, transform 0.5s ease-in-out";
-  //thing.style.transition = "all 0.5s ease-out";
+    thing.style.transition = "left 0.5s ease-out, top 0.5s ease-out, transform 0.5s ease-in-out";
+    //thing.style.transition = "all 0.5s ease-out";
 
-  //thing.style.transform = `scale(${0.5}) rotateZ(360deg)`;
+    //thing.style.transform = `scale(${0.5}) rotateZ(360deg)`;
 
-  thing.style.transform = `scale(${0.5})`;
+    thing.style.transform = `scale(${0.5})`;
 
-  thing.style.backgroundSize = `cover`;
-  thing.style.backgroundRepeat = `no-repeat`;
-  thing.style.backgroundPosition = `center`;
+    thing.style.backgroundSize = `cover`;
+    thing.style.backgroundRepeat = `no-repeat`;
+    thing.style.backgroundPosition = `center`;
 
-  //thing.style.border = '1px solid red';
+    //thing.style.border = '1px solid red';
 
-  let thingTop = person.offsetTop - thing.offsetHeight;
-  //let thingTop = person.offsetTop - thing.offsetWidth;
-  thing.style.top = thingTop + 'px';
+    let thingTop = person.offsetTop - thing.offsetHeight;
+    //let thingTop = person.offsetTop - thing.offsetWidth;
+    thing.style.top = thingTop + 'px';
 
 
-  //thing.style.backgroundColor = thingBgColor;
-  thing.style.zIndex = `10`;
+    //thing.style.backgroundColor = thingBgColor;
+    thing.style.zIndex = `10`;
 
-  thing.style.backgroundImage = `url('./img/${fotka}')`;
+    thing.style.backgroundImage = `url('./img/${fotka}')`;
 
-  thing.style.visibility = pokaz;
+    thing.style.visibility = pokaz;
+  }  
 }
 
 //Створення блоків завдань
 function TasksBlocks(){
+  if(FlagStart){
 
-  arrWagons.forEach((tb, index)=>{
-    tb = document.createElement('div');
-    tb.classList.add('tasksblocks_animationtravel');
-    scene.appendChild(tb);
+    arrWagons.forEach((tb, index)=>{
+      tb = document.createElement('div');
+      tb.classList.add('tasksblocks_animationtravel');
+      scene.appendChild(tb);
 
-    tb.style.position = 'absolute';
-    tb.style.display = `flex`;
-    tb.style.justifyContent = 'center';
-    tb.style.alignItems =  'center';
+      tb.style.position = 'absolute';
+      tb.style.display = `flex`;
+      tb.style.justifyContent = 'center';
+      tb.style.alignItems =  'center';
 
-    tb.style.left = 100*(pmin * 0.018)+'px';
-    tb.style.top = 1*(pmin * 0.01)+'px';
+      //tb.style.left = 100*(pmin * 0.018)+'px';
+      //tb.style.top = 1*(pmin * 0.01)+'px';
+      tb.style.left = '100%';
+      tb.style.top = '0px';
 
-    tb.style.minWidth = 20*(pmin * 0.01)+'px';
-    tb.style.minHeight = 10*(pmin * 0.01)+'px';
+      tb.style.minWidth = 20*(pmin * 0.01)+'px';
+      tb.style.minHeight = 10*(pmin * 0.01)+'px';
 
-    tb.style.transform = `scale(${0.1})`;
+      tb.style.transform = `scale(${0.1})`;
 
-    tb.style.fontSize = `${0.015*pmax}px`;
+      tb.style.fontSize = `${0.015*pmax}px`;
 
-    tb.style.backgroundColor = 'white';
+      tb.style.backgroundColor = 'white';
 
-//    tb.style.transition = "all 1.5s ease-in-out";
-    tb.style.transition = "left 1.0s ease-in-out, transform 1.0s ease-in-out";
+  //    tb.style.transition = "all 1.5s ease-in-out";
+      tb.style.transition = "left 1.0s ease-in-out, transform 1.0s ease-in-out";
 
-    tb.style.border = '1px solid red';
-    tb.style.borderRadius = '5%';
+      tb.style.border = '1px solid red';
+      tb.style.borderRadius = '5%';
 
-    tb.innerHTML = arrWagons[index].content;
+      tb.innerHTML = arrWagons[index].content;
 
 
-  });  
+    });  
 
-  tasksblocks = document.querySelectorAll(`.tasksblocks_animationtravel`);
+    tasksblocks = document.querySelectorAll(`.tasksblocks_animationtravel`);
+  }  
 } 
 
 
@@ -279,57 +292,63 @@ function TaskMove(){
 };
 
 function PersonChange(fotka){
-  person.style.backgroundImage = `url('./img/${fotka}?${MyRandomNumber()}')`;
-  //person.style.backgroundImage = `url('./img/${fotka}?${MyRandomNumber}')`;
-  // Кидок предмета у ціль
-  setTimeout(() => {
-    TaskUdar();
-  }, 1500); //Не можна змінювати
+  if(FlagStart){
+    person.style.backgroundImage = `url('./img/${fotka}?${MyRandomNumber()}')`;
+    //person.style.backgroundImage = `url('./img/${fotka}?${MyRandomNumber}')`;
+    // Кидок предмета у ціль
+    setTimeout(() => {
+      TaskUdar();
+    }, 1500); //Не можна змінювати
+  }  
 };
 
 function TaskUdar(){
-  thing.style.visibility = `visible`;
-  thing.style.transform = `scale(${1}) rotateZ(360deg)`;
+  if(FlagStart){
+    thing.style.visibility = `visible`;
+    thing.style.transform = `scale(${1}) rotateZ(360deg)`;
 
-  thing.style.left = `${randomLeft}px`;
-  thing.style.top = `${randomTop}px`;
-  setTimeout(() => {
-    thing.style.left = person.offsetLeft + person.offsetWidth / 4 +'px';    
-    thing.style.top = person.offsetTop - thing.offsetHeight +'px';    
-    thing.style.visibility = 'hidden';
-    thing.style.transform = `scale(${0.5}) rotateZ(-360deg)`;
-
+    thing.style.left = `${randomLeft}px`;
+    thing.style.top = `${randomTop}px`;
     setTimeout(() => {
-      TaskRemove();
-    }, 10 /*getRandomNumber(10, 2000)*/); //Можна змінювати
-  }, 750); //Можна 750, але не треба змінювати
+      thing.style.left = person.offsetLeft + person.offsetWidth / 4 +'px';    
+      thing.style.top = person.offsetTop - thing.offsetHeight +'px';    
+      thing.style.visibility = 'hidden';
+      thing.style.transform = `scale(${0.5}) rotateZ(-360deg)`;
+
+      setTimeout(() => {
+        TaskRemove();
+      }, 10 /*getRandomNumber(10, 2000)*/); //Можна змінювати
+    }, 750); //Можна 750, але не треба змінювати
+  }  
 }  
 
 // -------------------------------------------------------------------------------------
 function TaskRemove(){
-  tasksblocks[numtask].style.backgroundColor = 'gray';
+  if(FlagStart){
 
-  tasksblocks[numtask].style.transform = `scale(${0})`;
-  //tasksblocks[0].style.left = scene.offsetWidth / 2 + 'px';
-  tasksblocks[numtask].style.left = scene.offsetLeft + 'px';
-  setTimeout(() => {
-    tasksblocks[numtask].style.visibility = 'hidden';
-    tasksblocks[numtask].style.left = 90*(pmin * 0.018)+'px';
-    tasksblocks[numtask].style.top = 1*(pmin * 0.01)+'px';
+    tasksblocks[numtask].style.backgroundColor = 'gray';
+
+    tasksblocks[numtask].style.transform = `scale(${0})`;
+    //tasksblocks[0].style.left = scene.offsetWidth / 2 + 'px';
+    tasksblocks[numtask].style.left = scene.offsetLeft + 'px';
     setTimeout(() => {
-      numtask++
-      if(isEven(numtask)){
-        thing.style.backgroundImage = `url('./img/items/papkaG.png')`;
-      } else {
-        thing.style.backgroundImage = `url('./img/items/paperG.png')`;
-      }
-      if(numtask == tasksblocks.length-1){
-        thing.style.backgroundImage = `url('./img/items/bootleG.png')`;
-      }
+      tasksblocks[numtask].style.visibility = 'hidden';
+      tasksblocks[numtask].style.left = 90*(pmin * 0.018)+'px';
+      tasksblocks[numtask].style.top = 1*(pmin * 0.01)+'px';
+      setTimeout(() => {
+        numtask++
+        if(isEven(numtask)){
+          thing.style.backgroundImage = `url('./img/items/papkaG.png')`;
+        } else {
+          thing.style.backgroundImage = `url('./img/items/paperG.png')`;
+        }
+        if(numtask == tasksblocks.length-1){
+          thing.style.backgroundImage = `url('./img/items/bootleG.png')`;
+        }
 
-
-      TaskMove();
-    }, getRandomNumber(1000, 2000)); //Можна змінювати діапазон від 1000 вверх
-  }, 1000); // Не можна змінювати
+        TaskMove();
+      }, getRandomNumber(1000, 2000)); //Можна змінювати діапазон від 1000 вверх
+    }, 1000); // Не можна змінювати
+  }  
 }
 // ************************************** КІНЕЦЬ ВЕРСІЇ №3 ******************************************************
